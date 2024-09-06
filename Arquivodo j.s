a80b2f85b9fddd4965ca4b46b79bdc4ecadc1a52
yagosantozx <yagosantos150409@gmail.com>
2024-09-06 11:54:41 -0300

    Create Arquivodo j.s

---
+++ Arquivodo j.s
@@ -0,0 +1,66 @@
+function setup() {
+  createCanvas(400, 400);
+}f
+
+function draw() {
+  background(220);
+}
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+function draw() {
+    background("white");
+    
+    rect(0, 10, 100, 150);
+}
+function draw() {
+    background("white");
+
+    fill("red");
+    rect(0, 10, 100, 150);
+}
+function draw() {
+    background("white");
+    
+    stroke("blue") 
+    fill("red") 
+    rect(0, 10, 100, 150)
+}
+function draw() {
+  background(220);
+  
+  stroke("blue");
+  fill("red");
+  rect(mouseX, mouseY, 100, 150);
+}
+function draw() {
+  background(220);
+  
+  stroke("blue");
+  fill("red");
+  
+  console.log(mouseIsPressed);
+  rect(mouseX, mouseY, 100, 150);
+}
+function setup() {
+  // cria uma tela de 400px de largura por 400px de altura
+  createCanvas(400, 400);
+}
+function setup() {
+  createCanvas(400, 400);
+  background("white")
+}
+
+function draw() {
+  stroke("#03A9F4");
+  fill("rgb(201,104,223)");
+  
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
